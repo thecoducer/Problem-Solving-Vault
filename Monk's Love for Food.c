e1faf8c96bc21879852eafcba3f9ef5bdb5a3f4f
Mayukh's ASUS Laptop <thecoducer@gmail.com>
2017-09-21 11:35:29 +0530

    update basic stack problem

---
+++ Monk's Love for Food.c
@@ -0,0 +1,59 @@
+//https://www.hackerearth.com/practice/data-structures/stacks/basics-of-stacks/tutorial/
+#include<stdio.h>
+#define MAX 1000
+typedef struct stack
+{
+    int top;
+    int arr[MAX];
+}stack;
+stack s;
+
+void push();
+void pop();
+
+int main(void)
+{
+    int i, qr, choice;
+
+    s.top=-1;
+
+    scanf("%d", &qr);
+    for(i=qr;i>=1;i--)
+    {
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+            pop();
+            break;
+
+            case 2:
+            push();
+            break;
+        }
+    }
+}
+void pop()
+{
+    if(s.top==-1)
+        printf("No Food");
+    else
+    {
+        s.top=s.top-1;
+        printf("\n%d", s.arr[s.top+1]);
+    }
+
+}
+void push()
+{
+    int data;
+    scanf("%d", &data);
+
+    if(s.top==(MAX-1))
+        printf("Pile can have no more plates.");
+    else
+    {
+        s.top=s.top+1;
+        s.arr[s.top]=data;
+    }
+}
