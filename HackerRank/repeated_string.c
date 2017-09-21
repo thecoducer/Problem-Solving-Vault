4743727fd70fb673cf3878532660b66c2f33477e
Mayukh's ASUS Laptop <thecoducer@gmail.com>
2017-09-21 15:54:51 +0530

    repeated string

--- HackerRank/repeated_string.c
+++ HackerRank/repeated_string.c
@@ -1,9 +1,28 @@
-//https://www.hackerrank.com/challenges/repeated-string/problem
+// https://www.hackerrank.com/challenges/repeated-string/problem
 
-#include<stdio.h>
-#include<alloc.h>
+#include <stdio.h>
+#include<string.h>
+#define MAX 100
 
 int main(void)
 {
-    long n;
+    char str[MAX];
+    long n, count=0;
+    int i;
+
+    gets(str);
+    scanf("%ld", &n);
+
+    for(i=0;i<strlen(str);i++)
+        if(str[i]=='a')
+            count++;
+
+    count*=(n/strlen(str));
+
+    for(i=0;i<n%strlen(str);i++)
+        if(str[i]=='a')
+            count++;
+
+    printf("%ld", count);
+    return 0;
 }
