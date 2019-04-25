fd7c7a5055049f35a37b6aeb74780d8a45daf512
thecoducer <mayukh5741@gmail.com>
2019-04-25 12:31:28 +0530

    update

---
+++ CodeChef/Practice/ICPC16A.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int t, x1, y1, x2, y2;
+
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+        if(x1==x2){
+            if(y1>y2){
+                printf("down\n");
+            }else{
+                printf("up\n");
+            }
+        }else if(y1==y2){
+            if(x1>x2){
+                printf("left\n");
+            }else{
+                printf("right\n");
+            }
+        }else{
+            printf("sad\n");
+        }
+    }
+    return 0;
+}
