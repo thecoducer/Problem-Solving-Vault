4c2511b8499ea84048b360b9772cccf60a953c4d
thecoducer <mayukh5741@gmail.com>
2019-08-23 13:13:55 +0530

    update

---
+++ LeetCode/binary-gap.java
@@ -0,0 +1,16 @@
+/** https://leetcode.com/problems/binary-gap/ */
+
+class Solution {
+    public int binaryGap(int N) {
+        int last = -1, ans = 0;
+        for (int i = 0; i < 32; ++i)
+            if (((N >> i) & 1) != 0) { //check ith is a set bit or not
+                if (last >= 0)
+                    ans = Math.max(ans, i - last);
+                last = i; //store last index
+            }
+        return ans;
+    }
+}
+
+/** O(log n) time and O(1) space **/
