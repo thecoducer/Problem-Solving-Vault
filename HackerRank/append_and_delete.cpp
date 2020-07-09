69693ea1bd437147588e3fd873ed21150e85be79
Mayukh Datta <mayukh5741@gmail.com>
2020-07-09 14:27:31 +0530

    update

---
+++ HackerRank/append_and_delete.cpp
@@ -0,0 +1,43 @@
+// https://www.hackerrank.com/challenges/append-and-delete/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string appendAndDelete(string s, string t, int k) {
+    int i = 0, s_len, t_len;
+
+    s_len = s.length();
+    t_len = t.length();
+
+    if(k >= (s_len+t_len)) {
+        return "Yes";
+    }
+
+    while(i < s_len && i < t_len) {
+        if(s[i] != t[i]) {
+            break;
+        }
+        i++;
+    }
+
+    k = k - (s_len+t_len) + (2 * i);
+
+    if(k < 0) {
+        return "No";
+    }else if (k % 2 == 0) {
+        return "Yes";
+    }
+
+    return "No";
+}
+
+int main()
+{
+    string s, t;
+    int k;
+    cin >> s;
+    cin >> t;
+    cin >> k;
+    cout << appendAndDelete(s, t, k) << endl;
+    return 0;
+}
