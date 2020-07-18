4461c23c3168892cf6558fc0c9104117fe1e6449
Mayukh Datta <mayukh5741@gmail.com>
2020-07-18 20:21:50 +0530

    update

--- CodeChef/JULY20B/CRDGAME.cpp
+++ CodeChef/JULY20B/CRDGAME.cpp
@@ -4,50 +4,50 @@
 using namespace std;
 #define MAX 1000000
 
-int sum_of_digits(unsigned long long n) {
-    unsigned long long sum = 0;
+int sum_of_digits(int n) {
+    int sum = 0;
 
     while(n != 0) {
         sum += n % 10;
         n = n / 10;
     }
-
     return sum;
 }
 
 int main() {
     int t, n;
-    unsigned long long chef_card, morty_card, chef_score = 0L, morty_score = 0L;
+    int chef_card, morty_card;
 
     cin >> t;
 
     while(t--) {
         cin >> n;
 
+        int chef_score = 0, morty_score = 0;
+
         for(int i=0;i<n;i++) {
             cin >> chef_card >> morty_card;
 
+            int sum_chef = sum_of_digits(chef_card);
+            int sum_morty = sum_of_digits(morty_card);
+
             if(sum_of_digits(chef_card) > sum_of_digits(morty_card)) {
                 chef_score++;
+            }else if(sum_chef < sum_morty){
+                morty_score++;
             }else{
+                chef_score++;
                 morty_score++;
             }
-
-            //cout << "cc" << sum_of_digits(chef_card) << endl;
-            //cout << "mm" << sum_of_digits(morty_card) << endl;
         }
 
         if(chef_score > morty_score) {
-            printf("0 %llu\n", chef_score);
+            printf("0 %d\n", chef_score);
         }else if(morty_score > chef_score) {
-            printf("1 %llu\n", morty_score);
+            printf("1 %d\n", morty_score);
         }else if(chef_score == morty_score) {
-            printf("2 %llu\n", chef_score);
+            printf("2 %d\n", chef_score);
         }
-
-        //printf("hhhhhh %llu %llu hhhhhh", chef_score, morty_score);
-
-        chef_score = morty_score = 0L;
     }
 
     return 0;
