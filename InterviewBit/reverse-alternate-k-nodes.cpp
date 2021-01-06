3632105a9ef59d9b091094a7ec644b5deb1a2e3b
thecoducer <mayukh5741@gmail.com>
2021-01-06 22:45:29 +0530

    update

--- InterviewBit/reverse-alternate-k-nodes.cpp
+++ InterviewBit/reverse-alternate-k-nodes.cpp
@@ -2,37 +2,36 @@ listnode* solve(listnode* head, int k) {
     int i;
     listnode *current = head, *prev = NULL, *next = NULL;
     
-    // base case
-    if(current == NULL) {
-        return NULL;
-    }
-    
     // reverse k nodes
-    for(i=0;i<k;i++) {
+    int count = 0;
+    while(current != NULL && count < k) {
         next = current->next;
         current->next = prev;
         prev = current;
         current = next;
+        count++;
     }
     
     // link the k reversed list to the remaining part of the original list
     // head is the last node of the k reversed linked list
-    head->next = current;
-    
-    if(current == NULL) {
-        return prev;
+    if(head != NULL) {
+        head->next = current;
     }
     
     // skip k nodes
-    for(i=0;i<k-1;i++) {
+    count = 0;
+    while(current != NULL && count < k-1) {
         current = current->next;
+        count++;
     }
     
     // we call solve() for the rest of the list
     // also, link the unreversed part with the reversed part
     // solve() will return prev when we have elements remaining
     // and, solve() will return NULL when we have reached the end of the list
-    current->next = solve(current->next, k);
+    if(current != NULL) {
+        current->next = solve(current->next, k);
+    }
     
     return prev;
 }
