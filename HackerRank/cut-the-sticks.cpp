25be5cc8681df7854bdd38544cd54f8fa60742eb
thecoducer <mayukh5741@gmail.com>
2021-01-19 00:35:51 +0530

    update

---
+++ HackerRank/cut-the-sticks.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void cutTheSticks(int n, int a[]) {
+    sort(a, a+n);
+    
+    cout << n << endl;
+    for(int i=1;i<n;i++) {
+        if(a[i] != a[i-1]) {
+            cout << n - i << endl;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++) {
+        cin >> a[i];
+    }
+    
+    cutTheSticks(n, a);
+}
