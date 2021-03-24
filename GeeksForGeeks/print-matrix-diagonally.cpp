ab9c656ab11a9450a47a3a2c0f244acec29bb2bd
Mayukh Datta <mayukh5741@gmail.com>
2021-03-25 02:35:38 +0530

    update

--- GeeksForGeeks/print-matrix-diagonally.cpp
+++ GeeksForGeeks/print-matrix-diagonally.cpp
@@ -1,9 +1,11 @@
 vector<int> matrixDiagonally(int mat[MAX][MAX], int n)
 {
      int i = 0, j = 0;
+     // initial direction is down to up
      bool isUp = true;
      vector<int> result;
      
+     // traverse all n*n elements
      for(int k = 0; k < n * n;) {
          
          if(isUp) {
@@ -13,12 +15,16 @@ vector<int> matrixDiagonally(int mat[MAX][MAX], int n)
                  k++;
              }
              
-             if(i < 0 && j <= n-1) {
+             // if i becomes less than 0
+             // when j is still less than n
+             // reset i to 0
+             if(i < 0 && j < n) {
                  i = 0;
              }
              
+             // if j is out of bounds
              if(j == n) {
-                 i += 2;
+                 i += 2; // adding two when i is negative and j is out of bounds
                  j--;
              }
          }else{
@@ -28,15 +34,20 @@ vector<int> matrixDiagonally(int mat[MAX][MAX], int n)
                  k++;
              }
              
-             if(j < 0 && i <= n-1) {
+             // if j becomes less than 0
+             // when i is still less than n
+             // reset j to 0
+             if(j < 0 && i < n) {
                  j = 0;
              }
              
+             // if i is out of bounds
              if(i == n) {
-                 j += 2;
+                 j += 2; // adding two when j is negative and i is out of bounds
                  i--;
              }
          }
+         // reverse the flag to change the direction
          isUp = !isUp;
      }
      
