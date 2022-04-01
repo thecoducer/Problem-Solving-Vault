8a0a432d4e71e051687b48106d2fe5192e0d3819
Mayukh Datta <mayukh5741@gmail.com>
2022-04-01 18:14:45 +0530

    update

--- LeetCode/top-k-frequent-elements.java
+++ LeetCode/top-k-frequent-elements.java
@@ -1,10 +1,17 @@
+// https://leetcode.com/problems/top-k-frequent-elements/
+
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-class Solution {
+class Solution1 {
+    /**
+     * 
+     * TC: O(k logN + N)
+     * SC: O(N)
+     */
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> countMap = new HashMap<>();
 
@@ -16,21 +23,61 @@ class Solution {
         // value represents their frequency
 
         PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>(
-            (a, b) -> a.getValue().equals(b.getValue()) ? Integer.compare(b.getKey(),a.getKey()) : Integer.compare(b.getValue(), a.getValue())
-            );
+                (a, b) -> a.getValue().equals(b.getValue()) ? Integer.compare(b.getKey(), a.getKey())
+                        : Integer.compare(b.getValue(), a.getValue()));
 
-        // the comparator logic written here will compare numbers if frequency for any two elements is same
+        // the comparator logic written here will compare numbers if frequency for any
+        // two elements are same
 
-        for(Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
             maxHeap.add(entry);
         }
 
         List<Integer> list = new ArrayList<>();
-        for(int i=0; i<k; i++) {
+        for (int i = 0; i < k; i++) {
             list.add(maxHeap.poll().getKey());
         }
 
         return list.stream().mapToInt(i -> i).toArray();
 
+    }
+}
+
+class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for (int v : nums) {
+            countMap.put(v, countMap.getOrDefault(v, 0) + 1);
+        }
+
+        // using frequency indexing
+        List<List<Integer>> freqList = new ArrayList<>();
+
+        for (int i = 0; i <= nums.length; i++) {
+            freqList.add(new ArrayList<Integer>());
+        }
+
+        countMap.forEach((num, freq) -> {
+            freqList.get(freq).add(num);
+        });
+
+        List<Integer> result = new ArrayList<>();
+        int count = 0;
+        // traverse the freq list from right
+        for (int i = nums.length; i >= 0; i--) {
+            for (int v : freqList.get(i)) {
+                result.add(v);
+                count++;
+                if (count == k) {
+                   return result.stream().mapToInt(e -> e).toArray();
+                }
+            }
+        }
+        return result.stream().mapToInt(e -> e).toArray();
     }
 }
