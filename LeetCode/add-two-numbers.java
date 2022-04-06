6969fdc5b07941af9c7ad74c2b12f41dd1096770
Mayukh Datta <mayukh5741@gmail.com>
2022-04-06 14:02:14 +0530

    update

---
+++ LeetCode/add-two-numbers.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/add-two-numbers/
+
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode dummyHead = new ListNode();
+        ListNode curr = dummyHead;
+        
+        while(l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            carry = sum / 10;
+            sum = sum % 10;
+            
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+        }
+        
+        return dummyHead.next;
+    }
+}
