be715d6daf4886e16549ae42a08e5531779d1ea8
Mayukh Datta <mayukh5741@gmail.com>
2022-04-06 14:00:34 +0530

    update

--- LeetCode/add-two-numbers-2.java
+++ LeetCode/add-two-numbers-2.java
@@ -1,44 +1,31 @@
-// https://leetcode.com/problems/add-two-numbers-ii/
-
-import java.util.Stack;
+// https://leetcode.com/problems/add-two-numbers/
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> first = new Stack<>();
-        Stack<Integer> second = new Stack<>();
-
-        // insert elements from LL to stack
-        while(l1 != null) {
-            first.push(l1.val);
-            l1 = l1.next;
-        }
-
-        while(l2 != null) {
-            second.push(l2.val);
-            l2 = l2.next;
-        }
-
         int carry = 0;
-        ListNode head = null;
-        while(!first.isEmpty() || !second.isEmpty() || carry != 0) {
+        ListNode dummyHead = new ListNode();
+        ListNode curr = dummyHead;
+        
+        while(l1 != null || l2 != null || carry != 0) {
             int sum = carry;
-
-            if(!first.isEmpty()) {
-                sum += first.pop();
+            
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-
-            if(!second.isEmpty()) {
-                sum += second.pop();
+            
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
-
+            
             carry = sum / 10;
             sum = sum % 10;
-
-            ListNode newNode = new ListNode(sum);
-            newNode.next = head;
-            head = newNode;
+            
+            curr.next = new ListNode(sum);
+            curr = curr.next;
         }
-
-        return head;
+        
+        return dummyHead.next;
     }
 }
