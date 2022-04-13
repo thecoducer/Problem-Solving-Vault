dcf38e635a7daaaa2673fe938d22dd8a5e5d16df
Mayukh Datta <mayukh5741@gmail.com>
2022-04-13 11:51:35 +0530

    update

---
+++ LeetCode/merge-two-sorted-lists.java
@@ -0,0 +1,48 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        // if any of the list is empty, return the other list
+        if(l1 == null) {
+            return l2;
+        }
+        if(l2 == null) {
+            return l1;
+        }
+        
+        ListNode head = null, tail = null;
+        
+        // setting the head
+        if(l1.val < l2.val) {
+            head = l1;
+            l1 = l1.next;
+        }else{
+            head = l2;
+            l2 = l2.next;
+        }
+        
+        tail = head;
+        
+        // iterating till any of the list gets exhausted
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+                tail = tail.next;
+            }else{
+                tail.next = l2;
+                l2 = l2.next;
+                tail = tail.next;
+            }
+        }
+        
+        // if any of the list gets exhausted, attach the other list to the tail
+        if(l1 == null) {
+            tail.next = l2;
+        }
+        if(l2 == null) {
+            tail.next = l1;
+        }
+        
+        return head;
+    }
+}
