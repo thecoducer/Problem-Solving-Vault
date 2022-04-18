a2e2698c39589dafa2dc1000ca179c0db4958508
Mayukh Datta <mayukh5741@gmail.com>
2022-04-18 22:19:57 +0530

    update

---
+++ LeetCode/first-unique-characters-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        
+        for(int i=0;i<s.length();i++) {
+            int index = s.charAt(i) - 'a';
+            freq[index]++;
+        }
+        
+        for(int i=0;i<s.length();i++) {
+            int index = s.charAt(i) - 'a';
+            if(freq[index] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
