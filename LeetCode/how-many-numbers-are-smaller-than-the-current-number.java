f35e1afcd8fe7240ef5986ba79a4746a35344ef4
Mayukh Datta <mayukh5741@gmail.com>
2022-04-25 20:37:51 +0530

    update

--- LeetCode/how-many-numbers-are-smaller-than-the-current-number.java
+++ LeetCode/how-many-numbers-are-smaller-than-the-current-number.java
@@ -8,23 +8,25 @@ class Solution {
         int[] temp = new int[101];
         
         // storing frequency of elements
+        // element indexed
         for(int i=0;i<nums.length;i++) {
             temp[nums[i]]++;
         }
         
         // sum of frequencies
+        // "how many numbers" are counted by prefix sum technique
         for(int i=1;i<temp.length;i++) {
             temp[i] += temp[i-1];
         }
         
-        // 
+        // getting the "smaller than the current number" count
         for(int i=0;i<nums.length;i++) {
             int v = nums[i];
             
             if(v == 0) {
                 nums[i] = 0;
             }else{
-                nums[i] = temp[v - 1];
+                nums[i] = temp[nums[i] - 1];
             }
         }
         
