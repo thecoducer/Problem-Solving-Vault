7f5eb37852d96125263c7469e311df445e80c4cf
Mayukh Datta <mayukh5741@gmail.com>
2022-05-12 22:11:03 +0530

    update

--- LeetCode/excel-sheet-column-title.java
+++ LeetCode/excel-sheet-column-title.java
@@ -1,4 +1,29 @@
+// intuitive solution
 class Solution {
+    public String convertToTitle(int colNum) {
+        StringBuilder sb = new StringBuilder();
+        
+        while(colNum > 0) {
+            int asciiIndex = colNum % 26;
+            
+            // edge case
+            if(asciiIndex == 0) {
+                asciiIndex = 26;
+                colNum -= 26;
+            }
+            
+            char c = (char) (64 + asciiIndex);
+            sb.insert(0, c);
+            
+            colNum /= 26;
+        }
+        
+        return sb.toString();
+    }
+}
+
+
+class Solution1 {
     public String convertToTitle(int columnNumber) {
         StringBuilder sb = new StringBuilder();
         
