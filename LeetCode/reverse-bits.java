ed888b1dfc8392c20c2127469362ce8e251ebd4a
Mayukh Datta <mayukh5741@gmail.com>
2022-05-17 21:40:23 +0530

    update

--- LeetCode/reverse-bits.java
+++ LeetCode/reverse-bits.java
@@ -6,15 +6,13 @@ public class Solution {
         // going through 32 bits
         for(int i=0;i<32;i++) {
             // right shifting the result and making room for new bits
-            res = res << 1;
-            
+            res <<= 1;
+
             // stamping the set bits in the result
-            if((n & 1) == 1) {
-                res = res + 1;
-            }
-            
+            res += (n & 1);
+    
             // left shifting the original number to discard the LSB
-            n = n >> 1;
+            n >>= 1;
         }
         
         return res;
