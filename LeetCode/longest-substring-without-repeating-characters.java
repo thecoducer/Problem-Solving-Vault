f2a64f159a1702dc18573a98f1b45b881eb0fb0d
Mayukh Datta <mayukh5741@gmail.com>
2022-05-20 22:26:33 +0530

    update

--- LeetCode/longest-substring-without-repeating-characters.java
+++ LeetCode/longest-substring-without-repeating-characters.java
@@ -7,17 +7,23 @@ class Solution {
         Set<Character> set = new HashSet<>();
         int i, j, maxLen;
         i = j = maxLen = 0;
+        
+        // i points to the start of the window
+        // and j points to the end of the window
 
         while (j < s.length()) {
             if (!set.contains(s.charAt(j))) {
                 set.add(s.charAt(j));
                 j++;
-                maxLen = Math.max(maxLen, set.size());
+                maxLen = Math.max(maxLen, (j - i));
             } else {
                 set.remove(s.charAt(i));
                 i++;
             }
         }
+        
+        // prints the longest substring
+        System.out.println(s.substring(i, j));
 
         return maxLen;
     }
