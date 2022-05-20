63d3d6648a42f04bab4c45dea52f6084b6c76690
Mayukh Datta <mayukh5741@gmail.com>
2022-05-20 21:23:19 +0530

    update

---
+++ LeetCode/longest-substring-without-repeating-characters.java
@@ -0,0 +1,23 @@
+class Solution {
+    // TC: O(N)
+    // SC: O(N)
+    // used sliding window technique
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int i, j, maxLen;
+        i = j = maxLen = 0;
+        
+        while(j < s.length()) {
+            if(!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
+                j++;
+                maxLen = Math.max(maxLen, set.size());
+            }else{
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        
+        return maxLen;
+    }
+}
