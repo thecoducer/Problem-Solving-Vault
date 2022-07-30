cde400d0c091bbd40c5baf81d948f4659a4f6b42
Mayukh Datta <mayukh5741@gmail.com>
2022-07-30 12:28:34 +0530

    update

---
+++ LeetCode/find-and-replace-pattern.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> result = new ArrayList<>();
+        
+        for(String s : words) {
+            if(checkIsomorphic(s, pattern)){
+                result.add(s);
+            }
+        }
+        
+        return result;
+    }
+    
+    public boolean checkIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
+
+        int m = s.length();
+        int n = t.length();
+
+        // if the length of both the strings don't match then they cannot be isomorphic strings
+        if(m != n) {
+            return false;
+        }
+
+        for(int i=0;i<m;i++) {
+            // if the character is already mapped
+            if(map.containsKey(s.charAt(i))) {
+                // if the mapped value of s[i] doesn't match t[i], it's not isomorphic
+                if(map.get(s.charAt(i)) != t.charAt(i)) {
+                    return false;
+                }
+                // if the character is not mapped
+            }else{
+                // if t[i] is already mapped to some other character in s
+                // test case: 
+                // s = "eggs"
+                // t = "adda"
+                if(set.contains(t.charAt(i))) {
+                    return false;
+                }
+
+                map.put(s.charAt(i), t.charAt(i));
+                set.add(t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
