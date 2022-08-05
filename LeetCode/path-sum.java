ec03fb2de2224f151bb2c9b0b1c958956b82b1ba
Mayukh Datta <mayukh5741@gmail.com>
2022-08-06 00:05:21 +0530

    update

---
+++ LeetCode/path-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) {
+            return false;
+        }
+        
+        if(root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+        
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
