bc14d8964223ae000d6eedaec0fcd975ea05013e
Mayukh Datta <mayukh5741@gmail.com>
2022-08-05 20:15:11 +0530

    update

---
+++ GeeksForGeeks/BFS-tree.java
@@ -0,0 +1,28 @@
+// https://practice.geeksforgeeks.org/problems/level-order-traversal/1
+
+class Solution
+{
+    static ArrayList<Integer> levelOrder(Node node) 
+    {
+        // BFS or Level Order Traversal of a Tree
+        LinkedList<Node> queue = new LinkedList<>();
+        ArrayList<Integer> bfs = new ArrayList<>();
+        
+        queue.add(node);
+        
+        while(!queue.isEmpty()) {
+            Node element = queue.poll();
+            bfs.add(element.data);
+            
+            if(element.left != null) {
+                queue.add(element.left);
+            }
+            
+            if(element.right != null) {
+                queue.add(element.right);
+            }
+        } 
+        
+        return bfs;
+    }
+}
