52167157c1246989e73fd088fbebf8fe6ccf80cc
Mayukh Datta <mayukh5741@gmail.com>
2022-08-07 01:27:57 +0530

    update

---
+++ GeeksForGeeks/DFS-matrix.java
@@ -0,0 +1,76 @@
+// https://www.geeksforgeeks.org/depth-first-traversal-dfs-on-a-2d-array/
+
+import java.util.*;
+
+class Solution {
+
+    private static final int ROW = 3;
+    private static final int COL = 3;
+    private static final int dROW[] = { 0, 1, 0, -1 };
+    private static final int dCOL[] = { -1, 0, 1, 0 };
+
+    static class Pair {
+        public int first;
+        public int second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    private static boolean isValid(int row, int col, boolean[][] visited) {
+        if (row < 0 || row >= ROW || col < 0 || col >= COL) {
+            return false;
+        }
+
+        if (visited[row][col]) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static List<Integer> dfs(int row, int col, int[][] matrix, boolean[][] visited) {
+        Stack<Pair> stack = new Stack<>();
+        List<Integer> dfs = new ArrayList<>();
+
+        stack.push(new Pair(row, col));
+
+        while (!stack.isEmpty()) {
+            Pair element = stack.pop();
+
+            row = element.first;
+            col = element.second;
+
+            if (!isValid(row, col, visited)) {
+                System.out.println("row = " + row + " col = " + col);
+                continue;
+            }
+
+            visited[row][col] = true;
+            dfs.add(matrix[row][col]);
+
+            for (int i = 0; i < 4; i++) {
+                int adjX = row + dROW[i];
+                int adjY = col + dCOL[i];
+                stack.push(new Pair(adjX, adjY));
+            }
+        }
+        return dfs;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { -1, 2, 3 },
+                { 0, 9, 8 },
+                { 1, 0, 1 }
+        };
+
+        boolean[][] visited = new boolean[ROW][COL];
+
+        dfs(0, 0, matrix, visited).forEach(data -> System.out.print(data + " "));
+
+        System.out.println();
+    }
+}
