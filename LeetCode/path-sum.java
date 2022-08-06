ea420c394f2285fa459d403cecf88adf7513bac6
Mayukh Datta <mayukh5741@gmail.com>
2022-08-06 12:55:02 +0530

    update

--- LeetCode/path-sum.java
+++ LeetCode/path-sum.java
@@ -1,13 +1,19 @@
 class Solution {
+    // the idea here is to traverse each node down a root-to-leaf path 
+    // and subtract each node's value from the targetSum
+    // so when we reach a leaf node we then check whether that node's value is equal to the current value of targetSum or not
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        // when it reaches beyond the leaf node
         if(root == null) {
             return false;
         }
         
+        // when it's the leaf node
         if(root.left == null && root.right == null && root.val == targetSum) {
             return true;
         }
         
+        // dfs
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
