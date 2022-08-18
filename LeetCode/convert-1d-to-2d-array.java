a9c29e5dbb0ba8caabb7039b7fcee8fce0731bd5
Mayukh Datta <mayukh5741@gmail.com>
2022-08-19 00:10:06 +0530

    update

---
+++ LeetCode/convert-1d-to-2d-array.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int len = original.length;
+
+        // m * n gives us the counts of all cells in the matrix
+        // if that count is not equal to the count of elements in the original array
+        // then it is impossible to construct a 2D array out of the 1D array
+        if (len != (m * n)) {
+            return new int[0][0];
+        }
+        
+        int result[][] = new int[m][n];
+
+        for (int i = 0; i < len; i++) {
+            result[i / n][i % n] = original[i];
+        }
+
+        return result;
+    }
+}
