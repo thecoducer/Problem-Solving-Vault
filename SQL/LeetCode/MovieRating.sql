3f55c16f9509556a1afd363de97493cc99a24cd2
Mayukh Datta <mayukh5741@gmail.com>
2024-12-14 15:39:29 +0530

    update

---
+++ SQL/LeetCode/MovieRating.sql
@@ -0,0 +1,18 @@
+(
+  SELECT u.name AS results
+  FROM MovieRating r LEFT JOIN Users u
+  ON (r.user_id = u.user_id)
+  GROUP BY r.user_id
+  ORDER BY COUNT(*) DESC, u.name
+  LIMIT 1
+)
+UNION ALL
+(
+  SELECT m.title AS results
+  FROM MovieRating r LEFT JOIN Movies m
+  ON (r.movie_id = m.movie_id)
+  WHERE r.created_at LIKE '2020-02%'
+  GROUP BY r.movie_id
+  ORDER BY AVG(r.rating) DESC, m.title
+  LIMIT 1
+)
