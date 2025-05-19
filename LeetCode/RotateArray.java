8906534a5f71590595134653ddc14400fc6066c8
Mayukh Datta <mayukh5741@gmail.com>
2025-05-20 01:20:23 +0530

    update

--- LeetCode/RotateArray.java
+++ LeetCode/RotateArray.java
@@ -1,7 +1,13 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k % n; // ensure k is within the range [0, n]
+        /**
+         * The rotation algorithm (the three-step reversal method) works by treating k 
+         * as the effective number of positions to rotate. If k were larger than n, 
+         * performing rotations k times would be redundant. By using k = k % n;, 
+         * we reduce k to its effective value within the range [0, n-1].
+         */
+        k = k % n;
         reverse(nums, 0, n - 1); // reverse the entire array
         reverse(nums, 0, k - 1); // reverse the first k elements
         reverse(nums, k, n - 1); // reverse the the rest of the elements after k
